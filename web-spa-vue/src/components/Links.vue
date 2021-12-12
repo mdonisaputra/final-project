2e2ba4415b5a81fb0ad8ecfd91631b82eb58ee4f
Mihdan Advani <mihdan31@gmail.com>
2021-12-12 18:14:32 +0700

    fixing alert

---
+++ web-spa-vue/src/components/Links.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row align="center">
+
+      <v-spacer class="hidden-sm-and-down" />
+
+      <v-col
+        cols="12"
+        md="auto"
+      >
+        <div class="text-body-1 font-weight-light pt-6 pt-md-0 text-center">
+          &copy; {{ (new Date()).getFullYear() }}, Made by Kelompok 13
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Links',
+
+    data: () => ({
+    }),
+  }
+</script>
+
+<style lang="sass" scoped>
+  a
+    color: inherit !important
+</style>
---
+++ web-spa-vue/src/store/modules/index.js
@@ -0,0 +1,5 @@
+export { default as app } from './app'
+export { default as sales } from './sales'
+export { default as user } from './user'
+export { default as alert } from './alert'
+export { default as dialog } from './dialog'
